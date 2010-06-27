fa952bdb6a153b2d1852206d70a469f20f5fd628
Matt Moriarity <matt.moriarity@gmail.com>
2010-06-27 01:07:05 -0400

    Updated remaining graphics in Pocket Docket for Retina Display. [#30 state:resolved]

--- Classes/Views/PDListProgressView.m
+++ Classes/Views/PDListProgressView.m
@@ -3,26 +3,47 @@
 @implementation PDListProgressView
 
 - (void)drawRect:(CGRect)rect {
+	CGFloat len = 32;
+	CGFloat len2;
+	if ([self respondsToSelector:@selector(contentScaleFactor)]) {
+		len2 = len * self.contentScaleFactor;
+	} else {
+		len2 = len;
+	}
+	
+	void * volatile fp = (void *)&(UIGraphicsBeginImageContextWithOptions);
+	if (fp != NULL) {
+		UIGraphicsBeginImageContextWithOptions(CGSizeMake(len, len), NO, 0.0);
+	} else {
+		UIGraphicsBeginImageContext(CGSizeMake(len, len));
+	}
 	CGContextRef ctx = UIGraphicsGetCurrentContext();
 	CGContextSetRGBFillColor(ctx, 255, 255, 255, 0);
 	CGContextFillRect(ctx, rect);
-
-	CGRect topHalf = CGRectMake(0, 0, 32, 32 * (1.0 - self.progress));
-	CGRect bottomHalf = CGRectMake(0, topHalf.size.height, 32, 32 * self.progress);
+	
+	CGRect topHalf = CGRectMake(0, 0, len, len * (1.0 - self.progress));
+	CGRect topHalfBig = CGRectMake(0, 0, len2, len2 * (1.0 - self.progress));
+	CGRect bottomHalf = CGRectMake(0, topHalf.size.height, len, len * self.progress);
+	CGRect bottomHalfBig = CGRectMake(0, topHalfBig.size.height, len2, len2 * self.progress);
 	
 	UIImage *image = [UIImage imageNamed:@"ListIcon.png"];
-	CGImageRef cgImage = CGImageCreateWithImageInRect(image.CGImage, topHalf);
+	CGImageRef cgImage = CGImageCreateWithImageInRect(image.CGImage, topHalfBig);
 	
 	image = [UIImage imageWithCGImage:cgImage];
 	[image drawInRect:topHalf];
 	CGImageRelease(cgImage);
 	
 	image = [UIImage imageNamed:@"ListIconDark.png"];
-	cgImage = CGImageCreateWithImageInRect(image.CGImage, bottomHalf);
+	cgImage = CGImageCreateWithImageInRect(image.CGImage, bottomHalfBig);
 	
 	image = [UIImage imageWithCGImage:cgImage];
 	[image drawInRect:bottomHalf];
 	CGImageRelease(cgImage);
+	
+	UIImage *completeImage = UIGraphicsGetImageFromCurrentImageContext();
+	UIGraphicsEndImageContext();
+	
+	[completeImage drawInRect:CGRectMake(0, 0, len, len)];
 }
 
 @end
